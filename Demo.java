8f9497b7a46b546a406c5f278a5113e57b2cea78
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-30 22:23:47 +0530

    non - static variable

---
+++ Demo.java
@@ -0,0 +1,9 @@
+
+public class Demo {
+    // int eid = 101;  non - static varible eid
+    public static void main(String[] args){
+        system.out.println(eid);
+    }
+}
+
+// error: non-static variable eid cannot be referenced from a static context
