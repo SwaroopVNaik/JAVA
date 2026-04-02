3c5544a8cf4a82c18f6de87d4fd3ab9fdb15804a
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-04-02 11:23:43 +0530

    program_for_primitve_datatype

---
+++ program_for_primitive_datatype.java
@@ -0,0 +1,21 @@
+// write a porgram to print the defualt value of primtive datatypes
+public class program_for_primitive_datatype {
+    static byte b;
+    static short s;
+    static int i;
+    static long l;
+    static float f; 
+    static double d; 
+    static char c; 
+    static boolean bb;
+    public static void main(String[] args) {
+        System.out.println(b); // 0
+        System.out.println(s); // 0
+        System.out.println(i); // 0
+        System.out.println(l); // 0
+        System.out.println(f); // 0.0
+        System.out.println(d); // 0.0
+        System.out.println(c); // ----
+        System.out.println(bb); // false
+    }
+}
