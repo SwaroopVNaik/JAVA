d38a4672dbe4309d61d20454b5b6f1dc3b74859b
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-04-24 13:18:15 +0530

    OOP_S Inheritance

---
+++ OOPS/Parent.java
@@ -0,0 +1,19 @@
+// Inheritance
+// default parent class !
+package OOPS;
+
+class Parent{
+    public void m1(){
+        System.out.println("Parent class m1() method");
+    }
+    public void m2(){
+        System.out.println("Parent class m2() method");
+    }
+}
+
+// Child Class inheriting poperties of parent class
+class Child extends Parent{
+    public void m3(){
+        System.out.println("Child class m3() method");
+    }
+}
