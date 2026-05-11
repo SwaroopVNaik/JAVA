00f4694438ff2e4a905461213720f179680ad335
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-11 20:52:46 +0530

    try_catch

---
+++ Exception_Handler/TestOne.java
@@ -0,0 +1,7 @@
+public class TestOne{
+    public static void main(String[] args) {
+        String ename=null;
+        System.out.println(ename.length());
+        System.out.println("GM");
+    }
+}
