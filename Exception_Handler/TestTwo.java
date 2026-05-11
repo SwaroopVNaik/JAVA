1ebf2e9221b072fff9eda52eef6b8e7af41f72e4
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-11 20:53:05 +0530

    printwriter

---
+++ Exception_Handler/TestTwo.java
@@ -0,0 +1,8 @@
+import java.io.PrintWriter;
+
+public class TestTwo{
+    public static void main(String[] args) {
+        PrintWriter pw=new PrintWriter("data.txt");
+        //write login
+    }
+}
